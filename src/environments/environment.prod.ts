d0d62cbbbd7604f13bfaf92bbe746d5cce0b39b0
unknown <israelhache27@gmail.com>
2022-06-14 14:38:31 -0500

    [FIX] - Angular Compiler Version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   // apiUrl: 'http://localhost:4200'
   // apiUrl: 'http://127.0.0.1:3333/api/'
   // apiUrl: 'http://192.168.1.95:3333/api/'
-  apiUrl: 'https://hub-qa.solidusystems.mx'
+  apiUrl: 'https://hub-qa.solidusystems.mx/api/'
 };
